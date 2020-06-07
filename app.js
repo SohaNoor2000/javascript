78a079317ba8c52cde396ac8ec38c828f6308112
SohaNoor2000 <64960864+SohaNoor2000@users.noreply.github.com>
2020-06-07 03:23:46 -0700

    Done
    JS Done

---
+++ app.js
@@ -0,0 +1,148 @@
+//                             chapter#1
+// Q#1
+// alert("welcome to my website");
+// //  Q#2
+// alert("Error! please enter a valid password.");
+// // Q#3
+// alert("welcome to js land...\n Happy coding");
+// // Q#4 is on indexpage;
+
+// // Q#5
+// console.log("Hello! i can run JS through my web browser's console.")
+//Q#6
+
+
+
+//                              CHAPTER # 2
+// Q#1
+// var username;
+// // Q#2
+// var username;
+// username="soha noor"
+// // Q#3
+// var message;
+// message="Hello World";
+// alert(message);
+// // Q#4
+// var name="soha Noor";
+// var age=19;
+// var qualification="web and mobile hybrid appllication developer";
+// alert(name);
+// alert(age +"years old");
+// alert(qualification);
+// Q#5
+//    var x="pizza";
+// for(var i=5;i >0;i--){
+//     for (var j=0;j<i;j++)
+//     {
+//         document.write(x[j]);
+//     }
+//     document.write("</br>");
+// }
+ 
+
+// Q#6
+// var email="example@gmail.com";
+// alert("my email address is " + email);
+// // Q#7
+// var book="A smarter way to learn javascript";
+// alert("I am trying to learn from the Book " + book);
+// // Q#8
+// document.write("Yah! I can write HTML content through Javascript \n")
+// Q#9
+// var x="▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬";
+// alert(x);
+// document.write(x);
+// 
+
+// //                           CHAPTER #3
+//// Q#1
+// var age=19;
+// alert("I am "+age+" years old");
+
+// // Q#2
+// var n=14;
+// alert("you have visited this site "+ n+ " times");
+
+// //Q#3
+// var birthyear=2000;
+// document.write("my birthyear is "+ birthyear+" </br>" + " Data type of my ddeclared variable is number ");
+
+// // Q#4
+//  var visitor_name="soha noor";
+//  var product_title="T-shirt(s)";
+// var quantity=5;
+//  console.log(visitor_name  + " ordered "+quantity+" " + product_title+" on xyz clothing store.");
+
+
+////                           CHAPTER #5
+//// QUESTION #1
+// var num1= +prompt("enter 1st number");
+// var num2= +prompt("enter 2nd number");
+// var sum=num1+num2;
+// document.write("sum of "+ num1 +" and "+num2+ " is "+ sum);
+
+//// QUESTION #2
+// var num1= +prompt("enter 1st number");
+// var num2= +prompt("enter 2nd number");
+// var sub=num1-num2;
+// document.write("subtraction of "+ num1 +" and "+num2+ " is "+ sub+"</br>");
+//  var mul=num1*num2;
+//  document.write("multiplication of "+ num1 +" and "+num2+ " is "+ mul+"</br>");
+// var div=num1/num2;
+//  document.write("division of "+ num1 +" and "+num2+ " is "+ div+"</br>");
+//  var mod=num1%num2;
+//  document.write("modulus of "+ num1 +" and "+num2+ " is "+ mod+"</br>");
+
+//// QUESTION #3
+// var x;
+// document.write("value of variable x after declaration is: "+x+"</br>");
+// x=5;
+// document.write("initial value is: "+ x+"</br>");
+// x++;
+// document.write("after increment value is: "+x+"</br>");
+// x=x+7;
+// document.write("value of the variable after adding 7 is: "+x+"</br>");
+// x--;
+// document.write("after decremnet value is: "+x+"</br>");
+// document.write("the remainder is: "+x%3+"</br>");
+
+//// QUESTION #4
+// var one_movie_ticket= 600;
+// document.write("total cost of buying 5 tickets to a movie is "+one_movie_ticket*5 + "PKR .</br>");
+
+//// QUESTION #5
+// var x=5;
+// document.write("table of 5</br> " )
+// document.write(x+" * 1 = "+x*1+"</br>");
+// document.write(x+" * 2 = "+x*2+"</br>");
+// document.write(x+" * 3 = "+x*3+"</br>");
+// document.write(x+" * 4 = "+x*4+"</br>");
+// document.write(x+" * 5 = "+x*5+"</br>");
+// document.write(x+" * 6 = "+x*6+"</br>");
+// document.write(x+" * 7 = "+x*7+"</br>");
+// document.write(x+" * 8 = "+x*8+"</br>");
+// document.write(x+" * 9 = "+x*9+"</br>");
+// document.write(x+" * 10 = "+x*10+"</br>");
+
+//// QUESTION #5
+// var c=prompt("enter the temperature in celcius");
+//  var f=(c*9/5)+32;
+//  document.write(c+"C is "+f+"F</br>");
+//  var F=prompt("enter the temperature in celcius");
+//  var C=(F-32)*5/9;
+//  document.write(F+"F is "+C+"C");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
